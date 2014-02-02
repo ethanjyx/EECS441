e4396102478e348d7d1805fa948879aec7efc964
tjnkys <yshuo@umich.edu>
2014-02-01 20:33:43 -0500

    add OperationManager

---
+++ CollabrifyTest/OperationManager.h
@@ -0,0 +1,28 @@
+//
+//  OperationManager.h
+//  CollabrifyTest
+//
+//  Created by Shuo Yang on 2/1/14.
+//  Copyright (c) 2014 Collabrify.it. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OperationDeque.h"
+
+@interface OperationManager : NSObject
+
+// Singleton class, since we need to operate the same stacks from different view controller and class.
++ (OperationManager *) getOperationManager;
+
+// Use for tracking all confirmed operations.
+@property (strong, nonatomic) Deque *confirmedOp;
+
+// Use for local unconfirmed operations.
+@property (strong, nonatomic) Deque *unconfirmedOp;
+
+// The stack that can redo.
+@property (strong, nonatomic) Deque *redoStack;
+
+
+
+@end
---
+++ CollabrifyTest/OperationManager.m
@@ -0,0 +1,17 @@
+//
+//  OperationManager.m
+//  CollabrifyTest
+//
+//  Created by Shuo Yang on 2/1/14.
+//  Copyright (c) 2014 Collabrify.it. All rights reserved.
+//
+
+#import "OperationManager.h"
+
+@implementation OperationManager
+
+@synthesize confirmedOp = _confirmedOp;
+@synthesize unconfirmedOp = _unconfirmedOp;
+@synthesize redoStack = _redoStack;
+
+@end
